2002368a944ae160a90fabe992c4f6c42ae76027
CZÖVEK András <andras.czovek@kybest.hu>
2013-08-06 23:43:07 +0200

    Ticket clicking AJAX

--- sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
+++ sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
@@ -9,9 +9,9 @@
 <div class="mez-concert-hall" style="position: relative; width:<?php print $concert_hall_details['xsize'];?>px; height:<?php print $concert_hall_details['ysize'];?>px;">
 <?php print $concert_hall_details['markup']; ?>
 
-  <?php foreach($seats as $pid=> $seat): ?>
-    <div class="mez-ticket mez-ticket-<?php print $pid . ' ' . $seat['status'];?>" style="position:absolute; top: <?php print $seat['xcoord'];?>px; left: <?php print $seat['ycoord'];?>px;">
-      <a href="mez-ticket-to-basket/<?php print $concert_nid; ?>/<?php print $pid;?>"></a>
+  <?php foreach($seats as $pid => $seat): ?>
+    <div id="mez-ticket-"<?php print $pid; ?> class="mez-ticket <?php print $seat['status']; ?>" style="position:absolute; top: <?php print $seat['xcoord'];?>px; left: <?php print $seat['ycoord'];?>px;">
+      <?php print $seat['link']; ?>
     </div>
   <?php endforeach; ?>
 
