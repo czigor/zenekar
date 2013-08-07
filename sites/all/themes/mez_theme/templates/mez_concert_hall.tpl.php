080c82e941a059c3c3a2449f8fd7525999be7dc9
CZÖVEK András <andras.czovek@kybest.hu>
2013-08-08 00:27:43 +0200

    Ticket cart AJAX

--- sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
+++ sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
@@ -10,7 +10,7 @@
 <?php print $concert_hall_details['markup']; ?>
 
   <?php foreach($seats as $pid => $seat): ?>
-    <div id="mez-ticket-"<?php print $pid; ?> class="mez-ticket <?php print $seat['status']; ?>" style="position:absolute; top: <?php print $seat['xcoord'];?>px; left: <?php print $seat['ycoord'];?>px;">
+    <div id="mez-ticket-<?php print $pid; ?>" class="mez-ticket <?php print $seat['status']; ?>" style="position:absolute; top: <?php print $seat['xcoord'];?>px; left: <?php print $seat['ycoord'];?>px;">
       <?php print $seat['link']; ?>
     </div>
   <?php endforeach; ?>
