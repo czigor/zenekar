598d81e94360d76b0031e732ed54777c497561e0
CZÖVEK András <andras.czovek@kybest.hu>
2013-08-14 21:56:36 +0200

    Adding/removing tickets to/from the cart

--- sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
+++ sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
@@ -5,14 +5,11 @@
  */
 ?>
 
-
 <div class="mez-concert-hall" style="position: relative; width:<?php print $concert_hall_details['xsize'];?>px; height:<?php print $concert_hall_details['ysize'];?>px;">
-<?php print $concert_hall_details['markup']; ?>
-
-  <?php foreach($seats as $pid => $seat): ?>
-    <div id="mez-ticket-<?php print $pid; ?>" class="mez-ticket <?php print $seat['status']; ?>" style="position:absolute; top: <?php print $seat['xcoord'];?>px; left: <?php print $seat['ycoord'];?>px;">
-      <?php print $seat['link']; ?>
-    </div>
-  <?php endforeach; ?>
-
+	<?php print $concert_hall_details['markup']; ?>
+	<?php if (!empty($seats)) : ?>
+	  <?php foreach($seats as $pid => $seat): ?>
+      <?php print $seat; ?>
+	  <?php endforeach; ?>
+	<?php endif;?>
 </div> <!-- .mez-concert-hall -->
