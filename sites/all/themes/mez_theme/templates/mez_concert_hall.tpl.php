dfbcd37738d76e4ed306439b64119b07f75a6665
CZÖVEK András <andras.czovek@kybest.hu>
2013-10-07 22:33:36 +0200

    Concert hall variables check

--- sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
+++ sites/all/themes/mez_theme/templates/mez_concert_hall.tpl.php
@@ -4,12 +4,13 @@
  *
  */
 ?>
-
-<div class="mez-concert-hall" style="position: relative; width:<?php print $concert_hall_details['xsize'];?>px; height:<?php print $concert_hall_details['ysize'];?>px;">
-	<?php print $concert_hall_details['markup']; ?>
-	<?php if (!empty($seats)) : ?>
-	  <?php foreach($seats as $pid => $seat): ?>
-      <?php print $seat; ?>
-	  <?php endforeach; ?>
-	<?php endif;?>
-</div> <!-- .mez-concert-hall -->
+<?php if (!empty($concert_hall_details) && !empty($seats)) : ?>
+  <div class="mez-concert-hall" style="position: relative; width:<?php print $concert_hall_details['xsize'];?>px; height:<?php print $concert_hall_details['ysize'];?>px;">
+    <?php print $concert_hall_details['markup']; ?>
+    <?php if (!empty($seats)) : ?>
+      <?php foreach($seats as $pid => $seat): ?>
+        <?php print $seat; ?>
+      <?php endforeach; ?>
+    <?php endif;?>
+  </div> <!-- .mez-concert-hall -->
+<?php endif; ?>
