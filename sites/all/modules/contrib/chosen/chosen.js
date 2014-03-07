137b40531f7ac7b05108b6268e18cf61eacae118
CZÖVEK András <andras.czovek@kybest.hu>
2014-03-07 16:32:21 +0100

    Chosen 2.0-beta2

--- sites/all/modules/contrib/chosen/chosen.js
+++ sites/all/modules/contrib/chosen/chosen.js
@@ -29,7 +29,13 @@
       };
 
       $(selector, context)
-        .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select, .wysiwyg, .draggable select[name$="[weight]"], .draggable select[name$="[position]"], .chosen-disabled') //disable chosen on field ui
+        // Disabled on:
+        // - Field UI
+        // - WYSIWYG elements
+        // - Tabledrag weights
+        // - Elements that have opted-out of Chosen
+        // - Elements already processed by Chosen
+        .not('#field-ui-field-overview-form select, #field-ui-display-overview-form select, .wysiwyg, .draggable select[name$="[weight]"], .draggable select[name$="[position]"], .chosen-disabled, .chosen-processed')
         .filter(function() {
           // Filter out select widgets that do not meet the minimum number of
           // options.
@@ -42,13 +48,13 @@
             return $(this).find('option').length >= minOptions;
           }
         })
-        .each(function() {
+        .once('chosen', function() {
           options = getElementOptions(this);
           $(this).chosen(options);
-      });
+        });
 
       // Enable chosen for widgets.
-      $('select.chosen-enabled, select.chosen-widget', context).each(function() {
+      $('select.chosen-enabled, select.chosen-widget', context).once('chosen', function() {
         options = getElementOptions(this);
         $(this).chosen(options);
       });
