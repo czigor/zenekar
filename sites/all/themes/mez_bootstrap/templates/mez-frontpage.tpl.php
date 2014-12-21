e1c8925a3564a7c6324ad83bf43a64cdf79e32ab
CZÖVEK András <andras.czovek@kybest.hu>
2014-12-21 23:07:04 +0100

    Frontpage structure, data and theming

--- sites/all/themes/mez_bootstrap/templates/mez-frontpage.tpl.php
+++ sites/all/themes/mez_bootstrap/templates/mez-frontpage.tpl.php
@@ -6,13 +6,43 @@
  * $content
  */
 ?>
-<!-- first section - Home -->
-<div id="home" class="home" style="background: url(<?php print $content[0]['bg']; ?>) no-repeat center center fixed; ">
-  <div class="text-vcenter">
-    <?php print render($content[0]['node']); ?>
-    <a href="#about" class="btn btn-default btn-lg">Continue</a>
+<!-- Opening section -->
+<div id="opening" class="front-section" style="background: url(<?php print $opening['bg']; ?>) no-repeat center center fixed; background-size: cover;">
+  <div class="title">
+    <h1><?php print $site_name; ?></h1>
+    <span><?php print $site_slogan; ?><span>
+  </div>
+  <div class="jumbotron jumbotron-inner">
+    <?php print render($opening['node']); ?>
   </div>
 </div>
-<!-- /first section -->
+<!-- /Opening section -->
 
 
+<!-- News section -->
+<div id="news" class="front-section">
+  <div class="jumbotron jumbotron-outer"> <?php print render($news['node']);?></div>
+</div>
+<!-- /News section -->
+
+
+<!-- About section  -->
+<div id="about" class="front-section" style="background: url(<?php print $about['bg'] ?>) no-repeat center center fixed; background-size: cover;">
+  <div class="jumbotron jumbotron-inner">
+    <?php print render($about['node']); ?>
+  </div>
+</div>
+<!-- /About section -->
+
+<!-- News section -->
+<div id="news2">
+  <div class="jumbotron jumbotron-outer"> <?php print render($news['node']);?></div>
+</div>
+<!-- /News section -->
+
+<!-- Archive section  -->
+<div id="archive" class="front-section" style="background: url(<?php print $archive['bg'] ?>) no-repeat center center fixed; background-size: cover;">
+  <div class="jumbotron jumbotron-inner">
+  </div>
+</div>
+<!-- /Archive section -->
