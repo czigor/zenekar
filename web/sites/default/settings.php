20749393c12a6ecd2187b298809d2fe8925c0998
CZÖVEK András <czovekandras@gmail.com>
2023-08-15 15:50:23 +0200

    Config

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -235,7 +235,7 @@ $databases = [];
  * directory in the public files path. The setting below allows you to set
  * its location.
  */
-# $settings['config_sync_directory'] = '/directory/outside/webroot';
+$settings['config_sync_directory'] = '../config/sync';
 
 /**
  * Settings:
@@ -813,15 +813,3 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
-$databases['default']['default'] = array (
-  'database' => 'zenekar9',
-  'username' => 'root',
-  'password' => 'a',
-  'prefix' => '',
-  'host' => 'localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
-  'driver' => 'mysql',
-  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
-);
-$settings['config_sync_directory'] = 'sites/default/files/config_xVZDEKGjzc81hRI1AgRwNcai2daznlKnuuBGZeibjRL1UQnpyjSqJ-ezGUH3D5vKciUpGdMWhA/sync';
