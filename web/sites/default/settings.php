dec703c28551616ff92e89f5752a88a5e8c51849
CZÖVEK András <czovekandras@gmail.com>
2026-07-06 20:27:41 +0200

    Core update

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -813,3 +813,9 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// Automatically generated include for settings managed by ddev.
+$ddev_settings = __DIR__ . '/settings.ddev.php';
+if (getenv('IS_DDEV_PROJECT') == 'true' && is_readable($ddev_settings)) {
+  require $ddev_settings;
+}
